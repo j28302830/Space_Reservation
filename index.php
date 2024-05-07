64f94c898b4b958e43324a4d5cd743bc3ecce090
user <user@Ubuntu-64bits.myguest.virtualbox.org>
2024-05-07 16:26:32 +0800

    index.php add nav bar

--- index.php
+++ index.php
@@ -14,15 +14,31 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User login system</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/main.css">
   <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
+  <div>
+    <header>
+      <ul class="nav nav-justified">
+      <li class="nav-item">
+        <a class="nav-link" href="#">查詢</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">新增預約</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="./logout.php" >登出</a>
+      </li>
+      </ul>
+  </header>
+   </div>
   <div class="container">
     <div class="alert alert-success my-5">
       Welcome ! You are now signed in to your account.
@@ -32,7 +48,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
       <div class="col-lg-5 text-center">
         <img src="./img/blank-avatar.jpg" class="img-fluid rounded" alt="User avatar" width="180">
         <h4 class="my-4">Hello, <?= htmlspecialchars($_SESSION["username"]); ?></h4>
-        <a href="./logout.php" class="btn btn-primary">Log Out</a>
+        
       </div>
     </div>
   </div>
--- registered.sql
+++ registered.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 07, 2024 at 01:01 AM
+-- Generation Time: May 07, 2024 at 03:39 PM
 -- Server version: 8.0.36-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.17
 
@@ -41,7 +41,7 @@ CREATE TABLE `blockdate` (
 
 CREATE TABLE `reservation` (
   `rid` int NOT NULL,
-  `uid` int NOT NULL,
+  `id` int NOT NULL,
   `sid` int NOT NULL,
   `pid` int NOT NULL,
   `rdate` date NOT NULL
@@ -55,9 +55,56 @@ CREATE TABLE `reservation` (
 
 CREATE TABLE `seats` (
   `sid` int NOT NULL,
-  `location` varchar(255) NOT NULL,
+  `location` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `num` int UNSIGNED NOT NULL,
   `socket` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `seats`
+--
+
+INSERT INTO `seats` (`sid`, `location`, `num`, `socket`) VALUES
+(1, 'A', 1, 0),
+(2, 'A', 2, 1),
+(3, 'A', 3, 1),
+(4, 'A', 4, 1),
+(5, 'A', 5, 0),
+(6, 'A', 6, 0),
+(7, 'A', 7, 1),
+(8, 'A', 8, 1),
+(9, 'A', 9, 0),
+(10, 'A', 10, 1),
+(11, 'A', 11, 0),
+(12, 'A', 12, 1),
+(13, 'A', 13, 0),
+(14, 'A', 14, 0),
+(15, 'A', 15, 0),
+(16, 'A', 16, 1),
+(17, 'A', 17, 0),
+(18, 'A', 18, 0),
+(19, 'A', 19, 1),
+(20, 'A', 20, 0),
+(21, 'B', 1, 1),
+(22, 'B', 2, 1),
+(23, 'B', 3, 1),
+(24, 'B', 4, 1),
+(25, 'B', 5, 0),
+(26, 'B', 6, 1),
+(27, 'B', 7, 1),
+(28, 'B', 8, 1),
+(29, 'B', 9, 0),
+(30, 'B', 10, 0),
+(31, 'B', 11, 0),
+(32, 'B', 12, 0),
+(33, 'B', 13, 1),
+(34, 'B', 14, 0),
+(35, 'B', 15, 1),
+(36, 'B', 16, 1),
+(37, 'B', 17, 0),
+(38, 'B', 18, 1),
+(39, 'B', 19, 0),
+(40, 'B', 20, 0);
 
 -- --------------------------------------------------------
 
@@ -70,6 +117,7 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
   `reg_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -77,8 +125,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `reg_date`) VALUES
-(10, 'M123140002', 'aa28302830@gmail.com', '$2y$10$/8b55CHCPSJsQlQ08xkjSu/yhfzA4rqP7qL3JQMLkO4YnvpbHym6i', '2024-05-06 19:36:50');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `is_admin`, `reg_date`) VALUES
+(10, 'M123140002', 'aa28302830@gmail.com', '$2y$10$/8b55CHCPSJsQlQ08xkjSu/yhfzA4rqP7qL3JQMLkO4YnvpbHym6i', 0, '2024-05-06 19:36:50');
 
 --
 -- Indexes for dumped tables
@@ -94,7 +142,9 @@ ALTER TABLE `blockdate`
 -- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`rid`);
+  ADD PRIMARY KEY (`rid`),
+  ADD KEY `id` (`id`),
+  ADD KEY `sid` (`sid`);
 
 --
 -- Indexes for table `seats`
@@ -122,13 +172,24 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT for table `seats`
 --
 ALTER TABLE `seats`
-  MODIFY `sid` int NOT NULL AUTO_INCREMENT;
+  MODIFY `sid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sid` FOREIGN KEY (`sid`) REFERENCES `seats` (`sid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
